4d63b6f01f5e0d6e912f1bf1a5fc79585f195c6c
victoriak94 <vkamila6@gmail.com>
2018-12-04 14:22:15 -0500

    Created delete and edit routes.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -33,4 +33,29 @@ class PicturesController < ApplicationController
     end
   end
 
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+
+    if @picture.save
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to "/pictures"
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root 'pictures#index'
   get 'pictures' => 'pictures#index'
 
   post 'pictures' => 'pictures#create'
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
 
   get 'pictures/:id' => 'pictures#show'
 
+  get 'pictures/:id/edit' => "pictures#edit"
+  patch 'pictures/:id' => "pictures#update"
+
+  delete 'pictures/:id' => 'pictures#destroy'
+  
 end
