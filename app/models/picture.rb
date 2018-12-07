7e4b47269512b5f8937522266bce0bac050d0c35
victoriak94 <vkamila6@gmail.com>
2018-12-07 17:00:14 -0500

    Validate elements of artist, title, and url. Show error messages on new picture form

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,7 @@
 
 
 class Picture < ApplicationRecord
+  validates :artist, presence: true
+  validates :title, length:{minimum: 3, maximum: 20}
+  validates :url, presence: true, uniqueness: true
 end
